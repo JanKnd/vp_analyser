9bc408c2a8560a31eb818fa8194c51e14d6dcd4d
JanKnd <134882705+JanKnd@users.noreply.github.com>
2024-01-24 22:27:13 +0100

    additional changes

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@ mod vp_parser;
 mod vp_update;
 mod timetable;
 mod analyse;
+mod paths;
 
 use crate::vp_parser::Vertretungsplan;
 use chrono::{TimeZone, Weekday};
 
 fn main() {
-   let mut a: Vec<char> = vec![];
-   a.push('a');
-   a.push('b');
-   a.push('c');
-   print!("{:?}",analyse::search_for_char_from_index_on(&a,&'d',Option::None))
+   //let mut a: Vec<char> = vec![];
+   //a.push('a');
+   //a.push('b');
+   //a.push('c');
+   //print!("{:?}",analyse::search_for_char_from_index_on(&a,&'d',Option::None))
+   print!("{:?}",paths::get_paths())
 }
 
 
--- target/debug/deps/Main.d
+++ target/debug/deps/Main.d
@@ -1,9 +1,10 @@
-C:\Users\janku\RustroverProjects\vp_analyser\target\debug\deps\Main.exe: src\main.rs src\vp_parser.rs src\vp_update.rs src\timetable.rs src\analyse.rs
+C:\Users\janku\RustroverProjects\vp_analyser\target\debug\deps\Main.exe: src\main.rs src\vp_parser.rs src\vp_update.rs src\timetable.rs src\analyse.rs src\paths.rs
 
-C:\Users\janku\RustroverProjects\vp_analyser\target\debug\deps\Main.d: src\main.rs src\vp_parser.rs src\vp_update.rs src\timetable.rs src\analyse.rs
+C:\Users\janku\RustroverProjects\vp_analyser\target\debug\deps\Main.d: src\main.rs src\vp_parser.rs src\vp_update.rs src\timetable.rs src\analyse.rs src\paths.rs
 
 src\main.rs:
 src\vp_parser.rs:
 src\vp_update.rs:
 src\timetable.rs:
 src\analyse.rs:
+src\paths.rs:
